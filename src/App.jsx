40cf1a67d88e73e1e7e304c39cb63c960ae32c7d
developerfaz <fazilmohd456@gmail.com>
2025-05-29 00:53:04 +0300

    Added new images and completed the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Search from "./components/search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('')
     return (
-        <div>
-            
-        </div>
+        <main>
+            <div className={"pattern"}>
+
+            </div>
+            <div className={"wrapper"}>
+                <header>
+                    <img src={"../public/hero.png"} alt="hero banner" />
+                    <h1>Find <span className={"text-gradient"}>Movies</span> You'll Enjoy Without the Hassle</h1>
+                </header>
+
+                {/*importing first component that is from search.jsx*/}
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+                <h1 className={"text-white"}>{searchTerm}</h1>
+            </div>
+        </main>
     );
 };
 
